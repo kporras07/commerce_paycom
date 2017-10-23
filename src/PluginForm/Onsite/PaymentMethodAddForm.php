fd67db9699d2fd949a87ac208f2c8be8b460c520
Kevin Porras <kporras07@gmail.com>
2017-10-23 16:39:51 -0600

    Initial commit.

---
+++ src/PluginForm/Onsite/PaymentMethodAddForm.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\commerce_paycom\PluginForm\Onsite;
+
+use Drupal\commerce_payment\PluginForm\PaymentMethodAddForm as BasePaymentMethodAddForm;
+use Drupal\Core\Form\FormStateInterface;
+
+class PaymentMethodAddForm extends BasePaymentMethodAddForm {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function buildCreditCardForm(array $element, FormStateInterface $form_state) {
+    $element = parent::buildCreditCardForm($element, $form_state);
+    return $element;
+  }
+
+}
